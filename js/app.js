af98df99d82d131405c38a2eae0ca5cdca680f81
ysmaili.freitas <ysmaili.freitas@azi.com.br>
2023-01-03 09:55:14 -0400

    Livro Learning Angular JS capitulo 1 ao 4

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* chapter2/app.js */
+'use strict';
+/* App Module */
+var helloWorldApp = angular.module('helloWorldApp', [
+  'ngRoute',
+  'helloWorldControllers'
+]);
+helloWorldApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'partials/main.html',
+      controller: 'MainCtrl'
+    }).when('/show', {
+      templateUrl: 'partials/show.html',
+      controller: 'ShowCtrl'
+    });
+    $locationProvider.html5Mode(false).hashPrefix('!');
+  }]);
---
+++ js/controllers.js
@@ -0,0 +1,15 @@
+/* chapter2/controllers.js */
+'use strict';
+/* Controllers */
+var helloWorldControllers =
+  angular.module('helloWorldControllers', []);
+helloWorldControllers.controller('MainCtrl',
+  ['$scope', '$location', '$http',
+    function MainCtrl($scope, $location, $http) {
+      $scope.message = "Hello World";
+    }]);
+helloWorldControllers.controller('ShowCtrl',
+  ['$scope', '$location', '$http',
+    function ShowCtrl($scope, $location, $http) {
+      $scope.message = "Show The World";
+    }]);
