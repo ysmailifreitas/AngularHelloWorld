ae5a0a4b50d5fae5094cf77b4faf87dc6e8329a2
ysmaili.freitas <ysmaili.freitas@azi.com.br>
2023-01-03 12:33:52 -0400

    configurando REST service

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -2,31 +2,23 @@
 
 /* App Module */
 
-var helloWorldApp = angular.module('helloWorldApp', [
+var blogApp = angular.module('blogApp', [
   'ngRoute',
-  'helloWorldControllers'
+  'blogControllers',
+  'blogServices'
 
 ]);
 
 
-helloWorldApp.config(['$routeProvider', '$locationProvider',
+blogApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
     when('/', {
       templateUrl: 'partials/main.html',
-      controller: 'MainCtrl'
-    }).when('/show', {
-      templateUrl: 'partials/show.html',
-      controller: 'ShowCtrl'
-    }).when('/customer', {
-      templateUrl: 'partials/customer.html',
-      controller: 'CustomerCtrl'
-    }).when('/addCustomer', {
-      templateUrl: 'partials/newCustomer.html',
-      controller: 'AddCustomerCtrl'
-    }).when('/addedCustomer/:customer/:city', {
-      templateUrl: 'partials/addedCustomer.html',
-      controller: 'AddedCustomerCtrl'
+      controller: 'BlogCtrl'
+    }).when('/blogPost/:id', {
+      templateUrl: 'partials/blogPost.html',
+      controller: 'BlogViewCtrl'
     });
 
     $locationProvider.html5Mode(false).hashPrefix('!');
--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -6,65 +6,37 @@ var blogControllers = angular.module('blogControllers', []);
 
 
 
-blogControllers.controller('BlogCtrl', ['$scope',
-  function BlogCtrl($scope) {
-    $scope.blogList = [
-      {
-        "_id": 1,
-        "date": 1400623623107,
-        "introText": "This is a blog post about AngularJS. We will cover how to build",
-        "blogText": "This is a blog post about AngularJS. We will cover how to build a blog and how to add comments to the blog post."
+blogControllers.controller('BlogCtrl', ['$scope', 'BlogList',
+  function BlogCtrl($scope, BlogList) {
+    $scope.blogList = [];
+    BlogList.get({},
+      function success(response) {
+        //alert($scope.challenge.question);
+        console.log("Success:" + JSON.stringify(response));
+        $scope.blogList = response;
+
       },
-      {
-        "_id": 2,
-        "date": 1400267723107,
-        "introText": "In this blog post we will learn how to build applications based on REST",
-        "blogText": "In this blog post we will learn how to build applications based on REST web services that contain most of the business logic needed for the application."
+      function error(errorResponse) {
+        console.log("Error:" + JSON.stringify(errorResponse));
       }
-    ];
+    );
 
 
   }]);
 
-blogControllers.controller('BlogViewCtrl', ['$scope', '$routeParams',
-  function BlogViewCtrl($scope, $routeParams) {
+blogControllers.controller('BlogViewCtrl', ['$scope', '$routeParams', 'BlogPost',
+  function BlogViewCtrl($scope, $routeParams, BlogPost) {
     var blogId = $routeParams.id;
-    var blog1 = {
-      "_id": 1,
-      "date": 1400623623107,
-      "introText": "This is a blog post about AngularJS. We will cover how to build",
-      "blogText": "This is a blog post about AngularJS. We will cover how to build a blog and how to add comments to the blog post.",
-      "comments" :[
-        {
-          "commentText" : "Very good post. I love it."
+    $scope.blg = 1;
+    BlogPost.get({id: blogId},
+      function success(response) {
+        //alert($scope.challenge.question);
+        console.log("Success:" + JSON.stringify(response));
+        $scope.blogEntry = response;
         },
-        {
-          "commentText" : "When can we learn services."
-        }
-
-      ]
-    };
-
-    var blog2 = {
-      "_id": 2,
-      "date": 1400267723107,
-      "introText": "In this blog post we will learn how to build applications based on REST",
-      "blogText": "In this blog post we will learn how to build applications based on REST web services that contain most of the business logic needed for the application.",
-      "comments" :[
-        {
-          "commentText" : "REST is great. I want to know more."
-        },
-        {
-          "commentText" : "Will we use Node.js for REST services?"
-        }
-
-      ]
-    };
-
-    if(blogId === '1'){
-      $scope.blogEntry = blog1;
-    }else if(blogId === '2'){
-      $scope.blogEntry = blog2;
-    }
+      function error(errorResponse) {
+        console.log("Error:" + JSON.stringify(errorResponse));
+      }
+    );
 
   }]);
